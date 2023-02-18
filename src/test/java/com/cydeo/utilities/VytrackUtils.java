44f6c39888c80a0e4c240c7d1628123dcd5f8409
bereket <bkimesfin12@gmail.com>
2023-02-18 10:38:50 -0600

    cucumber junit bdd project is created

---
+++ src/test/java/com/cydeo/utilities/VytrackUtils.java
@@ -0,0 +1,12 @@
+package com.cydeo.utilities;
+
+public class VytrackUtils {
+//We will use project common areas
+ public   static void clickSubMenu(String main,String subMenu){
+
+ }
+ public static void clickMain(String main){
+     String locator="//ul[@id='top-menu']/li[contains(.,'"+main+"')]";
+
+ }
+}
